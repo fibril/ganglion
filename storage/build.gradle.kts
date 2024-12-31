dd615c37074a70d14b2fa95ee7ee3dffde8fdb72
Kizito Egeonu <kizito.egeonu@gmail.com>
2024-12-31 20:36:53 +0100

    Remove flyway

--- storage/build.gradle.kts
+++ storage/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     kotlin("jvm") version "1.9.0"
-    alias(libs.plugins.flyway) apply false
     alias(libs.plugins.shadow)
     id("ganglion.default")
 }
@@ -8,12 +7,9 @@ plugins {
 
 dependencies {
     api(libs.pg.client)
-    api(libs.mysql.client)
     implementation(libs.vertx.config)
     implementation(libs.vertx.kotlin.coroutines)
     implementation(libs.kotlinx.coroutines.core)
-    implementation(libs.flyway.core)
-    implementation(libs.flyway.pg)
     testImplementation(kotlin("test"))
 }
 
